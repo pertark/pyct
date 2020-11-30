bfafd279aa2a05d18b06776726477ad058683b31
EvilMuffinHa <hkailadka88@gmail.com>
2020-11-30 15:04:38 -0500

    spaces

--- src/modules/hello.py
+++ src/modules/hello.py
@@ -4,11 +4,11 @@ import click
 @click.command("hello")
 @click.option("--name", "-n", help="The person to greet", required=False)
 def hello(name):
-	if name:
-		click.echo('Hello %s!' % name)
-	else:
-		click.echo("Hello world! ")
+  if name:
+    click.echo('Hello %s!' % name)
+  else:
+    click.echo("Hello world! ")
 
 
 def load():
-	return [hello]
+  return [hello]
